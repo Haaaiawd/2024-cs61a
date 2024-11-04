c297adaef8746167ac7c3a72b07fbe09d2d36a63
haa <haayy@foxmail.com>
2024-11-04 10:39:38 +0800

    first commit

---
+++ lab01/1.py
@@ -0,0 +1,18 @@
+def middle(a, b, c):
+    """Return the number among a, b, and c that is not the smallest or largest.
+    Assume a, b, and c are all different numbers.
+
+    >>> middle(3, 5, 4)
+    4
+    >>> middle(30, 5, 4)
+    5
+    >>> middle(3, 5, 40)
+    5
+    >>> middle(3, 5, 40)
+    5
+    >>> middle(30, 5, 40)
+    30
+    """
+    return sorted([a,b,c][1:-1])
+a,b,c  = map(int,input().split())
+print(middle(a,b,c))
---
+++ text.py
@@ -0,0 +1,18 @@
+def middle(a, b, c):
+    """Return the number among a, b, and c that is not the smallest or largest.
+    Assume a, b, and c are all different numbers.
+
+    >>> middle(3, 5, 4)
+    4
+    >>> middle(30, 5, 4)
+    5
+    >>> middle(3, 5, 40)
+    5
+    >>> middle(3, 5, 40)
+    5
+    >>> middle(30, 5, 40)
+    30
+    """
+    return sorted([a,b,c])[1]
+a,b,c = 2,6,5
+assert middle(a,b,c) == 5, "haha,代码写错了555"
